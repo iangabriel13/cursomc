a4895d9f5cbf6aed4c4179571e864b04da2623c6
Ian Gabriel <42757211+iangabriel13@users.noreply.github.com>
2025-02-05 19:01:59 +0000

    Testing the first domain class - Category

--- src/main/java/com/naitech/cursomc/controllers/CategoryController.java
+++ src/main/java/com/naitech/cursomc/controllers/CategoryController.java
@@ -1,15 +1,29 @@
 package com.naitech.cursomc.controllers;
 
+
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.naitech.cursomc.domain.Category;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryController {
 
 	@GetMapping
-	public String list() {
-		return "REST is working!";
+	public List<Category> list() {
+		Category category1 = new Category(1, "Informatica");
+		Category category2 = new Category(2, "Escritorio");
+		
+		List<Category> list = new ArrayList<>();
+		list.add(category1);
+		list.add(category2);
+		
+		return list;
 	}
 }
