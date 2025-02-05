7901fc9be72769e47d58bd473abd3bd08af85910
Ian Gabriel <42757211+iangabriel13@users.noreply.github.com>
2025-02-05 20:37:45 +0000

    Fixing cyclical reference and Creating Exception Handler

---
+++ src/main/java/com/naitech/cursomc/controllers/exception/ControllerExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.naitech.cursomc.controllers.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.naitech.cursomc.services.exceptions.ObjectNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		StandardError error = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+		
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+}
--- src/main/java/com/naitech/cursomc/services/CategoryService.java
+++ src/main/java/com/naitech/cursomc/services/CategoryService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import com.naitech.cursomc.domain.Category;
 import com.naitech.cursomc.repositories.CategoryRepository;
+import com.naitech.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoryService {
@@ -19,7 +20,8 @@ public class CategoryService {
 	public Category find(Integer id) {
 		Optional<Category> category = categoryRepository.findById(id);
 
-		return category.get();
+		return category.orElseThrow(() -> new ObjectNotFoundException( 
+				"Category not found! Id: " + id + ", Type: " + Category.class.getName())); 
 	}
 
 }
