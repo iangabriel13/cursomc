e93a9b882492bcfdf2daf059abe0542f64e1f0d9
Ian Gabriel <42757211+iangabriel13@users.noreply.github.com>
2025-02-05 19:48:05 +0000

    Creating an instantiation operation

--- src/main/java/com/naitech/cursomc/CursomcApplication.java
+++ src/main/java/com/naitech/cursomc/CursomcApplication.java
@@ -1,13 +1,31 @@
 package com.naitech.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.naitech.cursomc.domain.Category;
+import com.naitech.cursomc.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Category category1 = new Category(null, "Informatica");
+		Category category2 = new Category(null, "Escritorio");
+		
+		categoryRepository.saveAll(Arrays.asList(category1, category2));
 	}
 
 }
