a6f46cb362509078343bc34bd4ef370668b3c84c
Ian Gabriel <42757211+iangabriel13@users.noreply.github.com>
2025-02-06 17:52:11 +0000

    POST /categories

--- src/main/java/com/naitech/cursomc/controllers/CategoryController.java
+++ src/main/java/com/naitech/cursomc/controllers/CategoryController.java
@@ -1,10 +1,15 @@
 package com.naitech.cursomc.controllers;
 
+import java.net.URI;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.naitech.cursomc.domain.Category;
 import com.naitech.cursomc.services.CategoryService;
@@ -24,5 +29,14 @@ public class CategoryController {
 		Category category = categoryService.find(id);
 
 		return ResponseEntity.ok().body(category);
+	}
+	
+	@PostMapping
+	public ResponseEntity<?> insert(@RequestBody Category category) {
+		category = categoryService.insert(category);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(category.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/naitech/cursomc/services/CategoryService.java
+++ src/main/java/com/naitech/cursomc/services/CategoryService.java
@@ -8,7 +8,10 @@ import com.naitech.cursomc.domain.Category;
 import com.naitech.cursomc.repositories.CategoryRepository;
 import com.naitech.cursomc.services.exceptions.ObjectNotFoundException;
 
+import jakarta.transaction.Transactional;
+
 @Service
+@Transactional
 public class CategoryService {
 
 	private CategoryRepository categoryRepository;
@@ -20,8 +23,13 @@ public class CategoryService {
 	public Category find(Integer id) {
 		Optional<Category> category = categoryRepository.findById(id);
 
-		return category.orElseThrow(() -> new ObjectNotFoundException( 
-				"Category not found! Id: " + id + ", Type: " + Category.class.getName())); 
+		return category.orElseThrow(() -> new ObjectNotFoundException(
+				"Category not found! Id: " + id + ", Type: " + Category.class.getName()));
+	}
+
+	public Category insert(Category category) {
+		category.setId(null);
+		return categoryRepository.save(category);
 	}
 
 }
